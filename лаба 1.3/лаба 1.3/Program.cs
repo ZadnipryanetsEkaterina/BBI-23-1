93ca148ca3bf489dab02656e09015a73d2fdd435
ZadnipryanetsEkaterina <146069453+ZadnipryanetsEkaterina@users.noreply.github.com>
2023-10-15 23:22:50 +0300

    Add files via upload

---
+++ лаба 1.3/лаба 1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double s = 0;
+        double y = 0;
+        double b = 1;
+        double a = 0.1;
+        double h = 0.05;
+        for (double x = a; x <= b; x += h)
+        {
+            s = 0;
+            int i = 1;
+            i = i * i;
+            while ((Math.Pow(2 * x, i) / i >= 0.0001))
+            {
+                s += (Math.Pow(2 * x, i) / i);
+                i += 1;
+            }
+            y = Math.Pow(Math.E, 2 * x);
+            Console.WriteLine();
+            Console.WriteLine($"{s}\t{y}");
+        }
+    }
+}
